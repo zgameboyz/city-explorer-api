72718fe260049d8a2652aedfbf8ecfe66959a1b7
Joel Connell <joel.connell@yahoo.com>
2021-06-19 10:48:00 -0700

    modularize some back end

---
+++ modules/Forecast.js
@@ -0,0 +1,30 @@
+
+
+
+// class Forecast {
+
+//   constructor(props){
+//     super(props);
+
+    
+//  this.valid_date=valid_date;
+//  this.description=description;
+
+//   }
+
+// }
+
+class Forecast {
+
+  constructor(date, description) {
+
+
+    this.date = date;
+    this.description = description;
+
+  }
+
+}
+
+module.exports = Forecast;
+
---
+++ modules/Movie.js
@@ -0,0 +1,12 @@
+class Movie {
+
+  constructor(title,release_Date) {
+
+
+    this.title = title;
+    this.release_Date = release_Date;
+  }
+
+}
+
+module.exports = Movie;
